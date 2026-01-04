3dd60d2cc8dfa44e205a830c7d5bafb209449ee7
Fahim Ahmed <ahmedfahimwork@gmail.com>
2026-01-04 20:15:27 +0530

    basic dashboard skeleton done

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import { UserButton } from "@clerk/nextjs"
+
 export const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 bg-green-300">
-        navbar
+      <div className="hidden lg:flex lg:flex-1 bg-fuchsia-600 pl-5">
+        search 
+      </div>
+        <UserButton/>
     </div>
   )
 }
